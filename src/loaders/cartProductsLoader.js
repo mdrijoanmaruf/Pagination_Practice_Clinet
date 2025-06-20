067b1a7763b4bf43d9c136a208a063e67f1f4e0c
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-21 03:39:50 +0600

    Fetch product by ides

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,29 +1,38 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProductsLoader = async () => {
-    const loadedProducts = await fetch('http://localhost:5000/products');
-    const products = await loadedProducts.json();
+  // if cart data is in database, you have to use async await
+  const storedCart = getShoppingCart();
+  const storedCartIdes = Object.keys(storedCart);
+  console.log(storedCartIdes);
 
-    // if cart data is in database, you have to use async await
-    const storedCart = getShoppingCart();
+  const loadedProducts = await fetch("http://localhost:5000/productsByIds", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(storedCartIdes),
+  });
+  const products = await loadedProducts.json();
+  console.log(loadedProducts);
 
-    const savedCart = [];
+  const savedCart = products;
 
-    for (const id in storedCart) {
-        const addedProduct = products.find(pd => pd._id === id);
-        if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-        }
-    }
+//   for (const id in storedCart) {
+//     const addedProduct = products.find((pd) => pd._id === id);
+//     if (addedProduct) {
+//       const quantity = storedCart[id];
+//       addedProduct.quantity = quantity;
+//       savedCart.push(addedProduct);
+//     }
+//   }
 
-    // if you need to send two things
-    // return [products, savedCart]
-    // another options
-    // return { products, cart: savedCart }
+  // if you need to send two things
+  // return [products, savedCart]
+  // another options
+  // return { products, cart: savedCart }
 
-    return savedCart;
-}
+  return savedCart;
+};
 
 export default cartProductsLoader;
